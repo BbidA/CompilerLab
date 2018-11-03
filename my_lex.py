69bdd3b04eb17c4321a3ac51b367385332e84ac5
liao <284983325@qq.com>
2018-11-03 22:55:59 +0800

    Complete the NFA construction function

--- my_lex.py
+++ my_lex.py
@@ -3,12 +3,21 @@
 # Integrate multi DFAs to a NFA
 # Transform NFA to DFA
 import re
-
+from collections import deque
 from functools import reduce
 
-_priority_table = {'•': 0, '|': 1, '*': 2, '+': 2, '?': 2}
+# ----------------------------------------------------------
+# transform regular expression to its postfix form
 
-_unary_operator = ('*', '+', '?')
+_cat = '•'
+
+_priority_table = {_cat: 0, '|': 1, '*': 2}
+
+_operators = (_cat, '|', '*', '+', '?', '(', ')', '{', '}')
+
+
+def _is_action(character: str):
+    return character not in _operators
 
 
 def to_postfix(regular_expr):
@@ -40,8 +49,8 @@ def to_postfix(regular_expr):
         else:
             while len(stack) != 0 and stack[-1] != '(' and _priority_table[stack[-1]] >= _priority_table[ch]:
                 postfix += stack.pop()
-            if ch in _unary_operator:
-                # unary operator just append to the postfix, no need
+            if ch == '*':
+                # star operator just append to the postfix, no need
                 # to push into the stack
                 postfix += ch
             else:
@@ -68,6 +77,9 @@ def _pre_process_regular_expr(regular_expr):
 
     """
     regular_expr = _process_square_brackets_expr(regular_expr)
+    regular_expr = _replace_brace_content(regular_expr)
+    regular_expr = _transform_plus_sign(regular_expr)
+    regular_expr = _transform_question_mark(regular_expr)
     return _add_cat_operator(regular_expr)
 
 
@@ -81,7 +93,7 @@ def _add_cat_operator(regular_expr):
         if curr_char == '(' or curr_char == '|':
             continue
         elif next_char.isalpha() or next_char.isdigit() or next_char == '(':
-            result += '•'
+            result += _cat
     result += regular_expr[-1]
     return result
 
@@ -102,3 +114,166 @@ def _process_square_brackets_expr(regular_expr):
         replacement = '(' + reduce(lambda a, b: a + '|' + b, result) + ')'
         regular_expr = regular_expr.replace(m, replacement)
     return regular_expr
+
+
+def _transform_plus_sign(regular_expr):
+    # todo
+    return regular_expr
+
+
+def _transform_question_mark(regular_expr):
+    # todo
+    return regular_expr
+
+
+def _replace_brace_content(regular_expr):
+    # todo
+    return regular_expr
+
+
+# ---------------------------------------------------------
+# construct NFA
+
+_epsilon = 'epsilon'
+
+
+class NFA:
+
+    def __init__(self):
+        self.start_node = None
+        self.final_nodes = []
+        self._map = {}
+        self._state_count = 0
+
+    def __getitem__(self, *state_and_action):
+        return self._map[state_and_action]
+
+    def __str__(self):
+        if self.start_node is None:
+            return 'start node not specified'
+
+        # implement a BFS to print nodes information
+        result = ''
+        nodes_queue = deque(self.start_node)
+        visited_nodes = []
+        while len(nodes_queue) != 0:
+            curr_state = nodes_queue.popleft()
+            visited_nodes.append(curr_state)
+            # this may need improved, the cost of this may be too high
+            for key in self._map.keys():
+                if curr_state in key:
+                    result += '{} -{}-> {}'.format(curr_state, key[1], self._map[key])
+                    if self._map[key] not in visited_nodes:
+                        nodes_queue.append(self._map[key])
+                result += '\n'
+        return result
+
+    def add_edge(self, source, action, target):
+        if self._map.get((source, action)) is None:
+            self._map[source, action] = set()
+
+        self._map[source, action].add(target)
+
+    def add_epsilon_edge(self, source, target):
+        self.add_edge(source, _epsilon, target)
+
+    def new_single_action(self, action):
+        head_node, last_node = self._allocate_nodes()
+
+        self.add_edge(head_node, action, last_node)
+        return head_node, last_node
+
+    def cat(self, nodes1, nodes2):
+        nodes1_tail = nodes1[1]
+        nodes2_head = nodes2[0]
+
+        # establish links between nodes1_tail and next states of nodes2_head
+        to_be_popped = []
+        to_be_append = {}
+        for state, action in self._map.keys():
+            if nodes2_head == state:
+                to_be_append[nodes1_tail, action] = self._map[state, action]
+                to_be_popped.append((state, action))
+        self._map.update(to_be_append)
+
+        # remove nodes2_head links to other nodes
+        for state_action in to_be_popped:
+            self._map.pop(state_action)
+
+        return nodes1[0], nodes2[1]
+
+    def new_or_nfa(self, a_nodes, b_nodes):
+        head_node, last_node = self._allocate_nodes()
+
+        self.add_epsilon_edge(head_node, a_nodes[0])
+        self.add_epsilon_edge(head_node, b_nodes[0])
+        self.add_epsilon_edge(a_nodes[1], last_node)
+        self.add_epsilon_edge(b_nodes[1], last_node)
+
+        return head_node, last_node
+
+    def new_star_nfa(self, nodes):
+        head_node, last_node = self._allocate_nodes()
+
+        self.add_epsilon_edge(head_node, nodes[0])
+        self.add_epsilon_edge(nodes[1], last_node)
+        self.add_epsilon_edge(nodes[1], nodes[0])
+        self.add_epsilon_edge(head_node, last_node)
+
+        return head_node, last_node
+
+    def _allocate_nodes(self):
+        head_node = self._state_count
+        last_node = self._state_count + 1
+        self._state_count += 2
+
+        return head_node, last_node
+
+
+def construct_nfa(regular_expr):
+    """Construct NFA for a regular expression
+
+    Parameters
+    ----------
+    regular_expr: str
+        regular expression which is used to construct this nfa
+
+    Returns
+    -------
+    nfa: dict
+        target NFA which is represented by a dictionary
+    """
+
+    postfix = to_postfix(regular_expr)
+    stack = []
+    nfa = NFA()
+
+    for ch in postfix:
+        if _is_action(ch):
+            # ch is an action
+            stack.append(nfa.new_single_action(ch))
+        elif ch == '*':
+            assert len(stack) > 0 and isinstance(stack[-1], tuple)
+            head, tail = stack.pop()
+            stack.append(nfa.new_star_nfa((head, tail)))
+        elif ch == '|':
+            assert len(stack) >= 2
+            nodes1 = stack.pop()
+            nodes2 = stack.pop()
+            stack.append(nfa.new_or_nfa(nodes1, nodes2))
+        elif ch == _cat:
+            assert len(stack) >= 2
+            nodes1 = stack.pop()
+            nodes2 = stack.pop()
+            # sequence of nodes1 and nodes2 is important here
+            stack.append(nfa.cat(nodes2, nodes1))
+        else:
+            raise ValueError('Illegal character: ' + ch)
+
+    # update nfa start state and final state
+    assert len(stack) == 1
+    head, tail = stack.pop()
+    nfa.start_node = head
+    nfa.final_nodes.append(tail)
+
+    return nfa
